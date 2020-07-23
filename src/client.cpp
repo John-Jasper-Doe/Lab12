c453d3163d99e09a3e1141a7ab112fccf6d0ad18
John Doe <john.jasper.doe@gmail.com>
2020-07-24 01:13:34 +0300

    Added of the client class

---
+++ src/client.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file client.cpp
+ * @brief Implimentation of the class "Client".
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+#include "client.hpp"
+
+namespace bulk_server {
+
+
+} /* bulk_server */
---
+++ src/client.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file client.hpp
+ * @brief Definition of the class "Client".
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+#ifndef CLIENT_HPP_
+#define CLIENT_HPP_
+
+namespace bulk_server {
+
+class client {};
+
+} /* bulk_server */
+
+#endif /* CLIENT_HPP_ */
