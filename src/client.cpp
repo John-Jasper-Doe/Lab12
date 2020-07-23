3aebb0a3b6355b506455fd2580fbd607ddfc8d16
John Doe <john.jasper.doe@gmail.com>
2020-07-24 01:27:31 +0300

    Added fields and constructors into client's class

--- src/client.cpp
+++ src/client.cpp
@@ -10,5 +10,7 @@
 
 namespace bulk_server {
 
+client::client(boost::asio::io_context& context, libasync::async::handle_t handle) noexcept
+  : context_(context), socket_(context_), handle_(handle) {}
 
 } /* bulk_server */
--- src/client.hpp
+++ src/client.hpp
@@ -9,9 +9,29 @@
 #ifndef CLIENT_HPP_
 #define CLIENT_HPP_
 
-namespace bulk_server {
+#include <boost/asio.hpp>
+#include <libasync/async/async.hpp>
 
-class client {};
+namespace bulk_server {
+namespace ba = boost::asio;
+
+class client {
+  ba::io_context& context_;
+  ba::ip::tcp::socket socket_;
+  libasync::async::handle_t handle_;
+
+public:
+  client() = delete;
+  explicit client(ba::io_context& context, libasync::async::handle_t handle) noexcept;
+
+  ~client() = default;
+
+  client(const client&) = delete;
+  client(client&&) = delete;
+
+  client& operator=(const client&) = delete;
+  client& operator=(client&&) = delete;
+};
 
 } /* bulk_server */
 
